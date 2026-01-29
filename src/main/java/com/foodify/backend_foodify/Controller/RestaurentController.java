1dc7d75b1e7a62e32f84042fbc29d860f3d2494a
chaitanyamahale2705-stack <chaitanyamahale2705@gmail.com>
2026-01-29 21:01:06 +0530

    Add files via upload
    Added restaurent endpoint

--- src/main/java/com/foodify/backend_foodify/Controller/RestaurentController.java
+++ src/main/java/com/foodify/backend_foodify/Controller/RestaurentController.java
@@ -2,7 +2,34 @@ package com.foodify.backend_foodify.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.foodify.backend_foodify.DTO.ApiResponse;
+import com.foodify.backend_foodify.Entities.Restaurent;
+import com.foodify.backend_foodify.Service.RestaurentService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
+
 public class RestaurentController {
+    
+    @Autowired
+    private RestaurentService restaurent_service;
+
+    @GetMapping("/restaurents")
+    public ResponseEntity<ApiResponse<List<Restaurent>>> getMethodName(@RequestParam int page) {
+        List<Restaurent> restaurents = restaurent_service.getallRestaurents(page);
+        ApiResponse<List<Restaurent>> response = new ApiResponse<>();
+        response.setData(restaurents);
+        response.setMessage("page "+page+" all restaurents fetch successfull");
+        response.setSuccess(true);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     
 }
--- src/main/java/com/foodify/backend_foodify/Service/RestaurentService.java
+++ src/main/java/com/foodify/backend_foodify/Service/RestaurentService.java
@@ -1,8 +1,26 @@
 package com.foodify.backend_foodify.Service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import com.foodify.backend_foodify.Entities.Restaurent;
+import com.foodify.backend_foodify.Repository.RestaurentRepo;
 
 @Service
 public class RestaurentService {
+
+    @Autowired
+    private RestaurentRepo restaurent_repo;
+
+    public List<Restaurent> getallRestaurents(int page) {
+
+        PageRequest pageable = PageRequest.of(page, 6);
+        Page<Restaurent> restaurents = restaurent_repo.findAll(pageable);
+        
+       return restaurents.getContent();
+    }
     
 }
